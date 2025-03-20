66af234af5a7636c17972f977339549bac6c8de7
Saugat Pandey <saugat.pandey@stud.h-da.de>
2025-03-20 13:40:35 +0100

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert(3 + 3)
+
+console.log(3 + 3)
+
+console.log("Saugat")
+
+let name = "Saugat"
+let age = 23
+let isLoggedIn = false
+let state;
+
+console.log(typeof undefined)
