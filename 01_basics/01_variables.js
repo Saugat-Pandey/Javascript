782cfe22df8d92202242b0f866c0141fb309195d
Saugat Pandey <saugat.pandey@stud.h-da.de>
2025-03-20 11:36:29 +0100

    JS_Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "saugat.pandey0209@gmail.com"
+var accountPassword = "12345"
+accountCity = "Darmstadt" 
+let accountState;
+
+//accountId = 2
+
+console.log(accountId);
+
+accountEmail = "saugat.pandey1705@gmail.com"
+accountPassword = "27634646545"
+accountCity = "Butwal"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
