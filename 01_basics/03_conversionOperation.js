0e3427ada8566cefd5ee223ec2432e2701f50705
Saugat Pandey <saugat.pandey@stud.h-da.de>
2025-03-20 14:20:29 +0100

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "Saugat"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "Saugat"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
